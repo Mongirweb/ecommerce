4291380fde9fac74ef485e10f06d27ba3be311b5
Mongirweb <mongirweb@gmail.com>
2025-03-06 10:44:49 -0500

    copia market 3

---
+++ utils/sendPurchaseConfirmationEmailSomos.js
@@ -0,0 +1,32 @@
+import { purchaseSuccedEmailTemplate } from "../emails/purchaseSuccedEmailTemplateSomos";
+import sendgrid from "@sendgrid/mail";
+sendgrid.setApiKey0(process.env.SENDGRID_API_KEY);
+const { EMAIL } = process.env;
+
+export async function sendPurchaseConfirmationEmailSomos(order, user) {
+  // Generate email content using the template
+
+  const emailTemplate = purchaseSuccedEmailTemplate({
+    orderNumber: order._id,
+    name: user.name,
+    products: order.products,
+    total: order.total,
+    shippingAddress: order.shippingAddress,
+  });
+
+  const msg = {
+    to: user.email, // User email
+    from: "notificaciones@somoselhueco.com", // Verified sender email
+    subject: `¡📦✅ Pedido confirmado! Somos el Hueco!`,
+    html: emailTemplate,
+  };
+
+  try {
+    await sendgrid.send(msg);
+  } catch (error) {
+    console.error(
+      "Error sending email:",
+      error.response?.body?.errors || error
+    );
+  }
+}
