e9b88b15898c867ef9bf45eb06a815c907d6bcca
Mongirweb <mongirweb@gmail.com>
2025-03-17 09:12:57 -0500

    update video home

--- data/videos.js
+++ data/videos.js
@@ -2,17 +2,17 @@ export const videos = [
   {
     platform: "tiktok",
     embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
-    videoId: "7337420502786477318",
+    videoId: "7412768412021165318",
   },
   {
     platform: "tiktok",
     embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
-    videoId: "7348526181710318853",
+    videoId: "7426346068226592005",
   },
   {
     platform: "tiktok",
     embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
-    videoId: "7358578637043453190",
+    videoId: "7441430715696254263",
   },
   {
     platform: "tiktok",
@@ -22,7 +22,34 @@ export const videos = [
   {
     platform: "tiktok",
     embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
-    videoId: "7359691506938023174",
+    videoId: "7449081828515859717",
+  }, //
+
+  {
+    platform: "tiktok",
+    embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
+    videoId: "7450138132898467078",
+  },
+
+  {
+    platform: "tiktok",
+    embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
+    videoId: "7459940857030937862",
+  },
+  {
+    platform: "tiktok",
+    embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
+    videoId: "7468793919531224326",
+  },
+  {
+    platform: "tiktok",
+    embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
+    videoId: "7481465743440186629",
+  },
+  {
+    platform: "tiktok",
+    embedUrl: "https://www.tiktok.com/@almacenmongir?_t=8&_r=1",
+    videoId: "7478051720719830278",
   },
   // Add more video objects here
 ];
